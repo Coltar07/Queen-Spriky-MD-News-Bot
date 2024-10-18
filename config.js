873dc6dfeeb4e564f21cf902a12b110908e2a485
Coltar07 <148589118+Coltar07@users.noreply.github.com>
2024-10-18 08:52:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    SESSION_ID: process.env.SESSION_ID || '8ntCzYCL#SWGdicpO67zKrqblnQQHeKlgkExvHfyYOLfCLFB8qko', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://Coltar07:<db_password>@coltar07.diwgx.mongodb.net/',    // Enter Your MongoDB URL
+    Owner: process.env.OwnerNumber || '2348109966247',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '2348209966247'    // Enter Your Bot Number
 };
